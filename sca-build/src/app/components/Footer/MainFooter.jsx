b26afcbdda26164d7aa7d120bb8fd03a28d6791d
Somadina <spark4daz@gmail.com>
2024-06-11 12:53:57 +0100

    footer test modification

--- sca-build/src/app/components/Footer/MainFooter.jsx
+++ sca-build/src/app/components/Footer/MainFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MainFooter() {
   return (
     <div className='container mx-auto'>
-        MainFooter gor
+        MainFooter for all
 
     </div>
   )
